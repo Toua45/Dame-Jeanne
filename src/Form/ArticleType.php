2a6f7242f3f5be3f5ae89f1316cf9e3268460eeb
Toua VA <toua.va45@gmail.com>
2020-02-04 14:36:08 +0100

    files about crud add article realised

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,19 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('author')
-            ->add('date')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('author', TextareaType::class, [
+                'label' => 'Auteur'
+            ])
+            ->add('date', DateTimeType::class, [
+                'label' => 'Date',
+                'data' => new DateTime(),
+            ])
         ;
     }
 
