d5e55baab3d42cb75e08537074164ed6c3cfc217
Lionel Delamare <lionel.delamare@hotmail.com>
2020-02-11 22:34:54 +0100

    Contact entity created - form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,8 +14,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ArticleRepository $articleRepository)
+    public function index(ArticleRepository $articleRepository, Request $request, MailerInterface $mailer)
     {
+
+
         $articles = $articleRepository->findBy([], ['date' => 'DESC']);
         return $this->render('home/index.html.twig', [
             'articles' => $articles,
