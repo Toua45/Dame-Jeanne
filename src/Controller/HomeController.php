4775ac4954f9d619260c59e39a30310dfb6eb690
Lionel Delamare <lionel.delamare@hotmail.com>
2020-03-18 13:14:51 +0100

    Admin modal home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
@@ -22,8 +23,13 @@ class HomeController extends AbstractController
      * @return Response
      * @throws TransportExceptionInterface
      */
-    public function index(ArticleRepository $articleRepository, Request $request, MailerInterface $mailer, CoordinateRepository $coordinateRepository)
+    public function index(ArticleRepository $articleRepository,
+                          Request $request,
+                          MailerInterface $mailer,
+                          CoordinateRepository $coordinateRepository,
+                          AuthenticationUtils $authenticationUtils)
     {
+
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
@@ -44,12 +50,17 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         $articles = $articleRepository->findBy([], ['date' => 'DESC'], 3);
         $coordinates = $coordinateRepository->findAll();
         return $this->render('home/index.html.twig', [
             'articles' => $articles,
             'form' => $form->createView(),
             'coordinates' => $coordinates,
+            'last_username' => $lastUsername,
         ]);
     }
+
 }
