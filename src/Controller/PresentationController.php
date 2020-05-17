9e2ef436585fcf6cf3a0058116c321f807b31ad2
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-05-17 19:23:18 +0200

    index et controller presentation avec mapping

---
+++ src/Controller/PresentationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TeamRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/presentation")
+ */
+class PresentationController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param TeamRepository $teamRepository
+     */
+    public function index(TeamRepository $teamRepository)
+    {
+        $teams = $teamRepository->findAll();
+
+        return $this->render('presentation/index.html.twig', [
+            'teams' => $teams,
+        ]);
+    }
+}
