b00da77a2f0dbdf2a8628f77b9ebab0a0640f2d1
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-05-18 19:17:37 +0200

    fix admin team et upload

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -3,22 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Team;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class TeamType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('description')
-            ->add('imageName')
-            ->add('updatedAt')
-        ;
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('description', CKEditorType::class, [
+                'label' => 'Parcours'
+            ])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Photo',
+                'required' => false,
+                'delete_label' => 'Supprimer l\'image ?',
+                'download_label' => 'Agrandir l\'image'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
