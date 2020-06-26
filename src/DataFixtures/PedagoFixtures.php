612902565881dea5ea1d316ff03984106a933684
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-06-26 18:11:00 +0200

    add PedagoFixtures

---
+++ src/DataFixtures/PedagoFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Pedago;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class PedagoFixtures extends Fixture
+{
+    Const PEDAGO = [
+        'Nature' => [
+            'title' => 'Nature?',
+            'content' => 'C\est comme ça que l\'on qualifie le vin que l\'on aime. Mais pourquoi ?',
+            'link' => '',
+        ]
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        foreach (self::PEDAGO as $pedago => $data) {
+            $pedago = new Pedago();
+            $pedago->setTitle($data['title']);
+            $pedago->setContent('content');
+            $pedago->setLink($faker->url);
+
+            $manager->persist($pedago);
+        }
+
+        $manager->flush();
+    }
+}
