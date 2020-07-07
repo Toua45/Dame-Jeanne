234ca4d4617703c07c00e266f91c838d1c9a04b4
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-07-07 22:20:51 +0200

    modification du pedagotype

--- src/Form/PedagoType.php
+++ src/Form/PedagoType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Pedago;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,16 @@ class PedagoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('link')
+            ->add('title', TextType::class, [
+                'label' => 'Mot de notre dico'
+            ])
+            ->add('content', CKEditorType::class, [
+                'label' => 'La définition',
+                'trim' => true,
+            ])
+            ->add('link', UrlType::class, [
+                'label' => 'Lien pour aller plus loin',
+            ])
         ;
     }
 
