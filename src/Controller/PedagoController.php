028df94c326051af1d197f6ab6fd43a0125aa670
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-07-07 22:15:08 +0200

    Mise en place de l'admin pour la page pedago

--- src/Controller/PedagoController.php
+++ src/Controller/PedagoController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PedagoController extends AbstractController
 {
     /**
-     * @Route("/pedagogie", name="pedago_index")
+     * @Route("/pedago", name="pedago_index")
      */
     public function index(PedagoRepository $pedagoRepository)
     {
