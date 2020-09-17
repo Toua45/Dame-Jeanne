b2845ea2fca02dcfa5f3f54951194f3ee1bfe3f0
Toua <toua.va45@gmail.com>
2020-09-17 10:27:29 +0200

    code about user manual on pdf realised

--- src/Controller/AdminHomeController.php
+++ src/Controller/AdminHomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+// Include Dompdf required namespaces
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class AdminHomeController extends AbstractController
 {
@@ -16,4 +19,37 @@ class AdminHomeController extends AbstractController
     {
         return $this->render('admin_home/index.html.twig');
     }
+
+    /**
+     * @Route("/admin/instructions", name="pdf_view")
+     */
+    public function viewPdf()
+    {
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('admin_home/pdf/user_manual_pdf.html.twig', [
+            'title' => "Guide d'utilisation"
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+
+
 }
