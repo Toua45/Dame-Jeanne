837b277d701b557ab6a79611fa04dfe27341af21
Lionel Delamare <lionel.delamare>
2020-11-14 11:21:57 +0100

    insert message into front, CSS and fix mobile aspects

---
+++ src/Controller/HeaderMessageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Alert;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HeaderMessageController extends AbstractController
+{
+    /**
+     * @Route("/header/message", name="header_message")
+     */
+    public function showAlertMessage()
+    {
+        $alerts = $this->getDoctrine()
+            ->getRepository(Alert::class)
+            ->findAll();
+
+        return $this->render('_alert_message.html.twig', [
+            'alerts' => $alerts,
+        ]);
+    }
+}
